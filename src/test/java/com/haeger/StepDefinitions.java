6aa0d68569b01a7052e02b9ee31ac56823154446
bbccd <c2p5@me.com>
2022-01-07 16:07:37 +0100

    Completely implemented all test cases from the former FilterTest.java class (TestNG/jUnit) in Cucumber. Removed FilterTest.java.

--- src/test/java/com/haeger/StepDefinitions.java
+++ src/test/java/com/haeger/StepDefinitions.java
@@ -1,6 +1,8 @@
 package com.haeger;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -16,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -26,16 +29,20 @@ public class StepDefinitions {
 
     String nodeURL;
 
-    @Given("the address book")
-    public void the_address_book() throws MalformedURLException {
+    @Before
+    public void openAddressBook() throws MalformedURLException {
         nodeURL = "http://192.168.0.4:4444";
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new RemoteWebDriver(new URL(nodeURL), chromeOptions);
+    }
+
+    @Given("the address book")
+    public void the_address_book() throws MalformedURLException {
         homePage = PageFactory.initElements(driver, com.haeger.AddressBookHomePage.class);
     }
 
-    @When("I filter for an existing user by last name")
-    public void i_filter_for_an_existing_user_by_last_name() {
+    @When("I filter for an existing customer by last name")
+    public void i_filter_for_an_existing_customer_by_last_name() {
 
         String searchTermExistingCustomerLastName = "Olsen";
         // wait for 1 s to allow page to load:
@@ -54,6 +61,58 @@ public class StepDefinitions {
         }
     }
 
+    @When("I filter for an existing customer by first name")
+    public void i_filter_for_an_existing_customer_by_first_name() {
+
+        String searchTermExistingCustomerFirstName = "Katelyn";
+        // wait for 1 s to allow page to load:
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // execute the search:
+        homePage.search(searchTermExistingCustomerFirstName);
+        // wait for 1 s to allow results to arrive:
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @When("I filter for a non-existent customer by a name that is an arbitrary UUID")
+    public void i_filter_for_a_non_existent_customer_by_a_name_that_is_an_arbitrary_UUID() {
+
+        // create UUID as name of customer guaranteed not to exist
+        String uuid = UUID.randomUUID().toString();
+
+        // wait for 1 s to allow page to load:
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // execute the search:
+        homePage.search(uuid);
+
+        // wait for 1 s to allow page to load:
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @And("reset the filter")
+    public void reset_the_filter() throws InterruptedException {
+        // reset search filter:
+        homePage.resetSearch();
+        // wait for 1 s to allow results to arrive:
+        Thread.sleep(1000);
+    }
+
     @Then("I should find at least one result")
     public void i_should_find_at_least_one_result() throws IOException {
         // assert that results were found:
@@ -62,11 +121,33 @@ public class StepDefinitions {
 
         // Create screenshot:
         File scrFile  = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("./target/screenshot_findExistingCustomerByLastName.png"));
+        FileUtils.copyFile(scrFile, new File("./target/screenshot_findExistingCustomerByName.png"));
+    }
+
+    @Then("I should find exactly 0 results")
+    public void i_should_find_exactly_0_results() throws IOException {
+        // assert that no results were found:
+        int numberOfResults = homePage.countFilterResults();
+        assertTrue(numberOfResults == 0);
+
+        // Create screenshot:
+        File scrFile  = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("./target/screenshot_filterNonExistingCustomerComesUpEmpty.png"));
     }
 
     @After()
     public void closeBrowser() {
         driver.quit();
+    }
+
+    @Then("I should find a large number of results in the results list")
+    public void i_should_find_a_large_number_of_results_in_the_results_list() throws IOException {
+        // assert that datasets are displayed again in list:
+        int numberOfResults = homePage.countFilterResults();
+        assertTrue(numberOfResults > 9);
+
+        // Create screenshot:
+        File scrFile  = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("./target/screenshot_resetFilterResetsResultsList.png"));
     }
 }
--- src/test/resources/com.haeger/filter.feature
+++ src/test/resources/com.haeger/filter.feature
@@ -1,7 +1,23 @@
 Feature: Finding People in Address Book
   Using the filter feature allows to find people in the address book
 
-  Scenario: Finding an existing person
+  Scenario: Finding an existing customer by last name
     Given the address book
-    When I filter for an existing user by last name
+    When I filter for an existing customer by last name
     Then I should find at least one result
+
+  Scenario: Finding an existing customer by last name
+    Given the address book
+    When I filter for an existing customer by first name
+    Then I should find at least one result
+
+  Scenario: Filtering for a non-existent customer returns an empty filter results list
+    Given the address book
+    When I filter for a non-existent customer by a name that is an arbitrary UUID
+    Then I should find exactly 0 results
+
+  Scenario: Filtering and then resetting the filter resets the display of the results list
+    Given the address book
+    When I filter for a non-existent customer by a name that is an arbitrary UUID
+    And reset the filter
+    Then I should find a large number of results in the results list
